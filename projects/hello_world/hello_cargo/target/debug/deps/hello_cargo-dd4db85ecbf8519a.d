a4627960854e0ce7a594ccfbd809ee1704ab29d9
Mohit Sharma <admin@mohitsharma.com>
2025-12-19 04:11:50 +0530

    first rust code

---
+++ projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a.d
@@ -0,0 +1,5 @@
+/Users/mohitsharma/Github/Rust/projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a.d: src/main.rs
+
+/Users/mohitsharma/Github/Rust/projects/hello_world/hello_cargo/target/debug/deps/hello_cargo-dd4db85ecbf8519a: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/mohitsharma/Github/Rust/projects/hello_world/hello_cargo/target/debug/hello_cargo: /Users/mohitsharma/Github/Rust/projects/hello_world/hello_cargo/src/main.rs
